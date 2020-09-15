8fc37cfa7ce78ce62cafaea8650c4c68514a7e58
Anna Romanovska <sup2a1nn@gmail.com>
2020-09-15 22:22:19 +0300

    Home page on Django

--- globalsolar/globalsolar/settings.py
+++ globalsolar/globalsolar/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'solar',
+    'solar_pages',
     'rest_framework',
+    'compressor',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'globalsolar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +121,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'globalsolar/static'),
+]
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
+
+COMPRESS_ROOT = os.path.join(BASE_DIR, 'globalsolar/static')
+COMPRESS_ENABLED = True
+COMPRESS_PRECOMPILERS = (
+    ('text/x-scss', 'django_libsass.SassCompiler'),
+)
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+MEDIA_URL = '/uploads/'
---
+++ globalsolar/globalsolar/static/CACHE/js/output.12a3c88e5c17.js
@@ -0,0 +1,1 @@
+function myMap(){let mapCanvas=document.querySelector(".mapContact");var myCenter=new google.maps.LatLng(50.4869837,30.4596262);var mapOptions={center:myCenter,zoom:14};let map=new google.maps.Map(mapCanvas,mapOptions);let marker=new google.maps.Marker({position:myCenter,animation:google.maps.Animation.BOUNCE,icon:"../img/img/Vector-Smart-Object_map.png"});marker.setMap(map);};
