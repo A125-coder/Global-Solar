e307ac8127dbd565ad5d0edf93bdb045bdc3c956
Anna Romanovska <sup2a1nn@gmail.com>
2020-09-17 22:28:00 +0300

    All pages added + URL Paths

--- globalsolar/globalsolar/static/js/main.js
+++ globalsolar/globalsolar/static/js/main.js
@@ -11,3 +11,72 @@ function myMap() {
   });
   marker.setMap(map);
 }
+
+let aboutCompany = document.querySelectorAll('.faq-question_0');
+let aboutCompanyP = document.querySelectorAll('.faq-question_');
+let aboutCompany_textP = document.querySelectorAll('.faq-question_1');
+
+  for (let i = 0; i < aboutCompany.length; i++) {
+
+    
+    aboutCompany[i].addEventListener('click', () => {
+      if (aboutCompany_textP[i].innerHTML == "") {
+      aboutCompany[i].style.height = "inherit";
+      aboutCompany[i].style.borderColor="#ffddc6";
+      aboutCompanyP[i].style.marginTop = "25px";
+      aboutCompany_textP[i].style.marginTop = "40px";
+        // aboutCompany_text.style.animation = "about_animation 2s 1";
+        switch (i) {
+            case 0:
+                aboutCompany_textP[i].innerHTML = 'З точки зору продуктивності,монокристалічні панелі демонструють кращі показники виробітку, ніж полікристалічні. Крім цього, дані фотодулі повільніше деградують. Але нижча ціна робить полікристалічні панелі економічно доцільнішими. Загальна вартість станції на основі полікристалічних панелей буде меншою, а от степінь виробітку буде залишатися на високому рівні.';
+                break;
+            case 1:
+                aboutCompany_textP[i].innerHTML = '«Зелений» тариф – це державна програма, яка стимулює розвиток відновлювальних джерел енергії. Держава зобов\'язується купувати згенеровану електроенергію у власників відновлювальних джерел енергії за фіксованою сумою.';
+                break;
+            case 2:
+                aboutCompany_textP[i].innerHTML = 'Станом на 2019 рік, ціна зеленого тарифу складає 0.18 євроцентів. При укладені угоди цього року, Ви продаватимете надлишки електроенергії державі за даною ціною до 2030 року.';
+                break;
+            case 3:
+                aboutCompany_textP[i].innerHTML = 'Звернутися в нашу компанію (: Спершу варто визначити технічне завдання ( Мета встановлення, тип станції, потужність, місце встановлення і ін. ) .';
+                break;
+            case 4:
+                aboutCompany_textP[i].innerHTML = 'Це зележить від потужності станції і комплектуючих. Станції потужністю в 30 кВт окупляються найшвидше. Приблизний термін окупності станції на 30 кВт – 4 роки.';
+                break;
+            case 5:
+                aboutCompany_textP[i].innerHTML = 'Можете, але дана процедура потребує спеціальних знань, тому краще скористатися послугами компанії, яка займається цим професійно.';
+                break;
+            case 6:
+                aboutCompany_textP[i].innerHTML = 'Ваша інвестиція залежить від потужності – вона може складати від 3 до 25 тис. $. Більші інвестиції – швидший і більший прибуток.';
+                break;
+            case 7:
+                aboutCompany_textP[i].innerHTML = 'Монокристалічні модулі - більше 30 років\n Полікристалічні модулі - більше 25 років\n Модулі з аморфного кремнію - 7-20 років.';
+                break;
+
+        }
+      }
+      else{
+        aboutCompany_textP[i].innerHTML = '';
+        aboutCompanyP[i].style.marginTop = "0";
+        aboutCompany[i].style.borderColor="#f2f2f2";
+        aboutCompany[i].style.height = "80px";
+      }
+    })
+}
+
+
+
+let imgProduct = [];
+
+for (let i = 0; i < 4; i++) {
+    imgProduct[i] = src = "../img/img/product_" + [i+1] + ".jpg";
+}
+for (let i = 0; i < 4; i++) {
+  document.querySelector('.product_img_'+[i+1]+'').src = imgProduct[i];
+}
+document.querySelector('.product_img_0').src = imgProduct[0];
+for (let i = 0; i < 4; i++) {
+  document.querySelector('.product_img_'+[i+1]+'').addEventListener('click', () => {
+    document.querySelector(".product_img_0").src = imgProduct[i];
+  })
+}
+
--- globalsolar/solar_pages/views.py
+++ globalsolar/solar_pages/views.py
@@ -18,20 +18,42 @@ def prices(request):
 def contacts(request):
     return render(request, 'pages/contacts.html')
 
+
 def dashboard(request):
     return render(request, 'pages/dashboard.html')
+
 
 def green_tarif(request):
     return render(request, 'pages/green_tarif.html')
 
+
 def register(request):
     return render(request, 'pages/register.html')
+
 
 def energy(request):
     return render(request, 'pages/energy.html')
 
+
 def our_works(request):
     return render(request, 'pages/our_works.html')
 
+
 def one_work(request):
     return render(request, 'pages/one_work.html')
+
+
+def catalog(request):
+    return render(request, 'pages/catalog.html')
+
+
+def product(request):
+    return render(request, 'pages/product.html')
+
+
+def ses(request):
+    return render(request, 'pages/ses.html')
+
+
+def  faq(request):
+    return render(request, 'pages/faq.html')
