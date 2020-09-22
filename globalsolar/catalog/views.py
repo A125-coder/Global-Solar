de7606cd6fe50a6c8ed79b9eb9e1388b36148b00
Forzik51 <supermaxxbox@gmail.com>
2020-09-22 19:50:06 +0300

    catalog product testCategory

--- globalsolar/catalog/views.py
+++ globalsolar/catalog/views.py
@@ -3,7 +3,7 @@ from .models import Catalog
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
-
+from django.db.models import Q
 
 def index(request):
     catalog_prod = Catalog.objects.all()
@@ -27,37 +27,37 @@ def product(request, product_id):
     return render(request, "pages/product.html", context)
 
 def SolarPanels(request):
-    SolarPanels = Catalog.objects.all().filter(catalog_type="Solar Panels monocrystalline")
+    SolarPanels = Catalog.objects.all().filter(Q(catalog_type='Solar Panels Monocrystalline') | Q(catalog_type='Solar Panels Polycrystalline'))
     print(SolarPanels)
     paginator = Paginator(SolarPanels, 9)
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
     print(page)
     context = {
-        "product": page,
+        "SolarPanels": page,
     }
-    return render(request, "pages/catalogPage.html", context)
+    return render(request, "pages/catalog/catalogPage.html", context)
 
 def monocrystalline(request):
-    catalog_prod = Catalog.objects.order_by(
-        '-list_date').filter(catalog_type='Solar Panels monocrystalline')
-    paginator = Paginator(catalog_prod, 9)
+    SolarPanels = Catalog.objects.all().filter(catalog_type='Solar Panels Monocrystalline')
+    print(SolarPanels)
+    paginator = Paginator(SolarPanels, 9)
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
-    
+    print(page)
     context = {
-        "product": page,
+        "SolarPanels": page,
     }
-    
-    return render(request, "pages/catalog.html", context)
+    return render(request, "pages/catalog/catalogMono.html", context)
     
 def polycrystalline(request):
-    catalog_prod = Catalog.objects.order_by(
-        '-list_date').filter(catalog_type='Solar Panels monocrystalline')
-    paginator = Paginator(catalog_prod, 9)
+    SolarPanels = Catalog.objects.all().filter(catalog_type='Solar Panels Polycrystalline')
+    print(SolarPanels)
+    paginator = Paginator(SolarPanels, 9)
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
+    print(page)
     context = {
-        "product": page,
+        "SolarPanels": page,
     }
-    return render(request, "pages/catalog.html", context)
+    return render(request, "pages/catalog/catalogPoly.html", context)
